d1b17d2ab59ba6c361befebd466a79918774bb8d
javalingappa <59753048+javalingappa@users.noreply.github.com>
2020-01-29 15:14:12 +0530

    Update ExamGeoModule.rb

--- modules/ExamGeoModule.rb
+++ modules/ExamGeoModule.rb
@@ -1,10 +1,10 @@
 require './Geo'
 #to modify the values in Modules use ::
 #Geo::PI=1
-puts Geo::Circle.area(2)
+puts Geo::Circle.area(2) # PI*r*r
 
-puts Geo::Circle.new.peri(3) #2PIr
+puts Geo::Circle.new.peri(3) # 2*PI*r
 
-puts Geo::Square.new.square(2)
+puts Geo::Square.new.square(2)#  s*s
 
-puts Geo::Square.new.peri(3) #2PIr
+puts Geo::Square.new.peri(3) # 4*s
